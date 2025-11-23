415caa7c27ef9ae8875d25a36f064bab3aff8794
Samiuddin <mdsamiuddin2005@outlook.com>
2025-11-23 19:25:36 +0530

    add system prompt for AI assistant and enhance prompting techniques

---
+++ hello-world/hello.md
@@ -0,0 +1,22 @@
+# the LLM model doesnt inheritedly have something called context you need to give it the data 
+
+## so thats why every llm model has a different context 
+![alt text](image.png)
+
+the GPT-5 nano has a context window of ![alt text](image-1.png)
+
+it is the cheapest model 
+so when we are doing like content stuff like the previous stuff the thing gets cached 
+
+Blogs and Projects
+
+try (we are orchestrating the AI) multi modal thinking like gemini will be doing the validating and openAI will be be doing 
+the thinking
+
+-anyalyse GEMINI 
+-thinking OPENAI
+-validting Another AI model 
+
+mix the persona based based prompting with the Chain of though prompting 
+
+
--- hello-world/main.py
+++ hello-world/main.py
@@ -4,13 +4,40 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = OpenAI()
+# System prompt techniques :=> used to make the AI specific to yours own thing
+# 1: One shot prompting / Zero Shot Prompting 
+# 2: Few Shot Prompting for every input we send a pre defined examples in the system prompts
+# 3: Chain of Thought Prompting => basically you re iterate over your solution basically thinking deeply
+# 4: Self Consistency prompting => make multiple answers of the thng from 3 models , and then ask from other other models about it , cost badegi magar accuracy badedi
+# 5: Persona-based Prompting => kisi ke jaise rehta like an individual (200 lines ke around rehta) 
+SYSTEM_PROMPT = """
+    You are an helpfull AI assistant who is spec in resolving user queries 
+    For the given user input , analyze the input and brak down the problem step by step
+    
+    the steps are you get the user input , you analyze , you think again and think for several times and then return the output with an simple explanation 
 
+    Follow the steps in sequence such as "analyse" , "think" , "output" , "validate" and finally result 
+    
+    Example:
+    Input: What is 2+2 
+    Output: {{"step" : "analyse" , }}
+    
+"""
+
+# COT project 
+
+# Home Work 
+#try (we are orchestrating the AI) multi modal thinking like gemini will be doing the validating and openAI will be be doing the thinking
+# anyalyse GEMINI
+# thinking OPENAI
+# validting Another AI model 
+
+# We are using the ChatML prompting techniques
 response = client.responses.create(
     model="gpt-5-nano",
     input=[
-        {"role":"user" , "content" : "Hey my name is Sami"} ,
-        {"role":"assistant" , "content" : "Hello Sami how are you"} ,
-        {"role" : "user" , "content" : "What is my name"}
+        # This is the system prompt which we are giving first 
+        {"role":"system" , "content" : SYSTEM_PROMPT},
     ]
 )
 
